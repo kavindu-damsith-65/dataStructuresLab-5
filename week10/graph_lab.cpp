4d5656e9a3845047d07238e05f86dc7c8aaaae58
kavindu-damsith-65 <122905907+kavindu-damsith-65@users.noreply.github.com>
2023-05-18 18:14:01 +0530

    week 10

---
+++ week10/graph_lab.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+struct Node
+{
+    int label;
+    list<int> neighbours;
+};
+
+struct Graph
+{
+    // graph will have an array of type "node" with length specified by n
+    int n = 8;
+    Node *nodes = new Node[n];
+
+    void intializenodes()
+    {
+        // iterate through the nodes and assign labels
+        for (int i = 0; i < n; i++)
+        {
+            nodes[i].label = i;
+        }
+    }
+
+    void addedge(int u, int v)
+    {
+        nodes[u - 1].neighbours.push_back(v);
+        nodes[v - 1].neighbours.push_back(u);
+    }
+
+    void print()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << "node " << nodes[i].label << " - neighbours: ";
+            for (int neighbour_node : nodes[i].neighbours)
+            {
+                cout << neighbour_node << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Graph *g = new Graph;
+    g->intializenodes();
+
+    g->addedge(1, 2);
+    g->addedge(1, 3);
+    g->addedge(1, 4);
+    g->addedge(1, 5);
+    g->addedge(2, 3);
+    g->addedge(2, 6);
+    g->addedge(4, 6);
+    g->addedge(4, 7);
+    g->addedge(4, 8);
+    g->addedge(5, 6);
+    g->addedge(5, 7);
+    g->addedge(5, 8);
+  
+    g->print();
+}
